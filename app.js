d458b25621c52aa085ddfd27163dab9aaf437b57
chemicalRatt <chemicalRatt@outlook.com>
2023-04-09 00:37:57 -0400

    added workaround for dragging interfereing with functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const BTN_SMALL = document.getElementById("small");
 const BTN_MEDIUM = document.getElementById("medium");
 const BTN_LARGE = document.getElementById("large");
 
+let mouseDown = false;
 function initializeGrid(size){
     const totalSize = (size*size);
     const containerSize = GRID_CONTAINER.clientWidth;
@@ -12,13 +13,27 @@ function initializeGrid(size){
     for (let i = 0; i < totalSize; i++){
         const div = document.createElement("div");
         div.style.flexBasis = containerSize/size + "px";
-        div.addEventListener("mouseenter", function(){
-            div.style.backgroundColor = "black";});
+        div.addEventListener("mousedown", function(){mouseDown = true;},false);
+        div.addEventListener("mouseup", function(){mouseDown = false;},false);
+        div.addEventListener("mouseenter", drawColor,false);
         GRID_CONTAINER.appendChild(div);
     }
+}
+
+function drawColor(e){
+    if(mouseDown){
+        e.target.style.backgroundColor = "black";
+    }
+
 }
 
 initializeGrid(16); //default
 BTN_SMALL.addEventListener("click",initializeGrid.bind(null,16),false);
 BTN_MEDIUM.addEventListener("click",initializeGrid.bind(null,32),false);
 BTN_LARGE.addEventListener("click",initializeGrid.bind(null,64),false);
+
+
+// Prevents dragging from interfering with functionality.
+document.addEventListener("dragstart",function(event) {
+    event.preventDefault();
+},false);
