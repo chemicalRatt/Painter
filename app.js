979d03f6efcc111ad13d2b31485fb815e0b001db
chemicalRatt <chemicalRatt@outlook.com>
2023-04-08 23:49:39 -0400

    add buttons to select size/clear canvas

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const GRID_CONTAINER = document.getElementById("grid-container");
+const BTN_SMALL = document.getElementById("small");
+const BTN_MEDIUM = document.getElementById("medium");
+const BTN_LARGE = document.getElementById("large");
 
 function initializeGrid(size){
     const totalSize = (size*size);
     const containerSize = GRID_CONTAINER.clientWidth;
+
+    GRID_CONTAINER.innerHTML = "";
 
     for (let i = 0; i < totalSize; i++){
         const div = document.createElement("div");
@@ -13,4 +18,7 @@ function initializeGrid(size){
     }
 }
 
-initializeGrid(64);
+initializeGrid(16); //default
+BTN_SMALL.addEventListener("click",initializeGrid.bind(null,16),false);
+BTN_MEDIUM.addEventListener("click",initializeGrid.bind(null,32),false);
+BTN_LARGE.addEventListener("click",initializeGrid.bind(null,64),false);
